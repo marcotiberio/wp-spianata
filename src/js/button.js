71cb1f445a43e76812a9b684a49480819b79068c
Marco Tiberio <tiberiomarco01@gmail.com>
2019-11-30 15:45:00 +0100

    test event

--- src/js/button.js
+++ src/js/button.js
@@ -1,19 +1,13 @@
-// document.getElementsByClassName('ajax_add_to_cart').addEventListener('click', function (clicked) {
-//     return function () {
-//         if (!clicked) {
-//             var last = this.innerHTML;
-//             this.innerHTML = 'Added';
-//             clicked = true;
-//             setTimeout(function () {
-//                 this.innerHTML = last;
-//                 clicked = false;
-//             }.bind(this), 1000);
-//         }
-//     };
-// }(false), this);
-
-$('.ajax_add_to_cart').on("click",function(){
-    $(this).val("Adding....");
-      setTimeout(function(){
-         $('.ajax_add_to_cart').val("Added");}, 2000);
- });
+document.getElementsByClassName('ajax_add_to_cart').addEventListener('click', function (clicked) {
+    return function () {
+        if (!clicked) {
+            var last = this.innerHTML;
+            this.innerHTML = 'Added';
+            clicked = true;
+            setTimeout(function () {
+                this.innerHTML = last;
+                clicked = false;
+            }.bind(this), 1000);
+        }
+    };
+}(false), this);
