4d9e232df6966e58f42bc9690ee732af57fc1a65
Marco Tiberio <tiberiomarco01@gmail.com>
2019-12-03 17:39:00 +0100

    restore

--- src/js/cycle.js
+++ src/js/cycle.js
@@ -1,15 +1,3 @@
 jQuery(document).ready(function ($) {
-    $("#spianata-title").hover( function() { $("#spianata-image").toggle(); } );
-});
-
-jQuery(document).ready(function ($) {
-    $("#catering-title").hover( function() { $("#catering-image").toggle(); } );
-});
-
-jQuery(document).ready(function ($) {
-    $("#classes-title").hover( function() { $("#classes-image").toggle(); } );
-});
-
-jQuery(document).ready(function ($) {
-    $("#instagram-title").hover( function() { $("#instagram-image").toggle(); } );
+    $("#spianata-title").hover( function() { $("#homepage-image").toggle(); } );
 });
