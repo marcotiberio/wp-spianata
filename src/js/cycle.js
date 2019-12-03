5927a6a22cf11d4cabcbca6c67dae49b76084d85
Marco Tiberio <tiberiomarco01@gmail.com>
2019-12-03 17:14:41 +0100

    banner test

--- src/js/cycle.js
+++ src/js/cycle.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function ($) {
     $("#spianata-title").hover( function() { $("#spianata-image").toggle(); } );
+    $("#catering-title").hover( function() { $("#catering-image").toggle(); } );
+    $("#classes-title").hover( function() { $("#classes-image").toggle(); } );
+    $("#instagram-title").hover( function() { $("#instagram-image").toggle(); } );
 });
