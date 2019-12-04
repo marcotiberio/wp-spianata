f8e05b6f67b2711d9c05272058ec24ae1689a2e7
Marco Tiberio <tiberiomarco01@gmail.com>
2019-12-04 12:28:50 +0100

    test home banners

--- src/js/cycle.js
+++ src/js/cycle.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
     $("#spianata-title").hover( function() { $("#spianata-image").toggle(); } );
-    $("#spianata-title").hover( function() { $("#catering-image").toggle(); } );
-    $("#spianata-title").hover( function() { $("#classes-image").toggle(); } );
-    $("#spianata-title").hover( function() { $("#instagram-image").toggle(); } );
+    $("#catering-title").hover( function() { $("#catering-image").toggle(); } );
+    $("#classes-title").hover( function() { $("#classes-image").toggle(); } );
+    $("#instagram-title").hover( function() { $("#instagram-image").toggle(); } );
 });
