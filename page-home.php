40e03fb36e1740b544357f55f9294ebe31e70a22
Marco Tiberio <tiberiomarco01@gmail.com>
2019-12-05 11:57:36 +0100

    test size video

--- page-home.php
+++ page-home.php
@@ -69,8 +69,8 @@ $imagemobile = get_field('mobile_image');
         </div>
         <div class="homepage-image" id="catering-image" style="background-image:url('<?php echo $imagecatering['url']; ?>'); background-repeat: no-repeat; background-size: cover;">
         </div>
-        <div class="homepage-image" id="classes-image">
-            <video width="100%" height="" autoplay loop muted>
+        <div class="homepage-video" id="classes-image" style="display: block;">
+            <video class="home-video" autoplay loop muted>
                 <source src="<?php the_field('classes_image');?>" type="video/mp4">
                 <source src="<?php the_field('classes_image');?>" type="video/ogg">
                 Your browser does not support the video tag.
