0ca1dc3bed7f29222355043d4e726e7849164239
Marco Tiberio <tiberiomarco01@gmail.com>
2019-12-13 11:50:05 +0100

    scroll

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -8,3 +8,26 @@ jQuery(document).ready(function ($) {
         }, 1000);
     });
 });
+
+jQuery(document).on( 'click', 'a[href^="#"]', function(e){
+    e.preventDefault();
+    var hash = jQuery(this).attr('href');
+    var el = jQuery(hash);
+    var top = el.offset().top;
+    jQuery('html, body').animate({
+        scrollTop: top
+    }, 300, 'swing',
+        function() {
+            window.location.hash = hash;
+        }
+    );
+} );
+
+window.laytheme.on('newpageshown', function(){
+    var hash = window.location.hash; 
+    var el = jQuery(hash);
+    if(el.length > 0) {
+        var top = el.offset().top;
+        window.scroll(0, top);        
+    }
+});
