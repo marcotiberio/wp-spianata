25d223df1345c0d3abbe03a858ecec23d3c1477f
Marco Tiberio <tiberiomarco01@gmail.com>
2019-12-14 15:36:11 +0100

    test

--- src/js/cycle.js
+++ src/js/cycle.js
@@ -5,10 +5,10 @@ jQuery(document).ready(function ($) {
     $("#instagram-title").hover( function() { $("#instagram-image").toggle(); } );
 });
 
-$('input:checkbox').change(function(){
-    if($(this).is(":checked")) {
-        $('div.shipping_address').addClass("showdifferentaddress");
-    } else {
-        $('div.shipping_address').removeClass("showdifferentaddress");
-    }
-});
+// $('input:checkbox').change(function(){
+//     if($(this).is(":checked")) {
+//         $('div.shipping_address').addClass("showdifferentaddress");
+//     } else {
+//         $('div.shipping_address').removeClass("showdifferentaddress");
+//     }
+// });
--- src/js/resize.js
+++ src/js/resize.js
@@ -1,11 +1,3 @@
 // window.onresize = function() {
 //     location.reload();
 // }
-
-$('input:checkbox').change(function(){
-    if($(this).is(":checked")) {
-        $('div.shipping_address').addClass("showdifferentaddress");
-    } else {
-        $('div.shipping_address').removeClass("showdifferentaddress");
-    }
-});
